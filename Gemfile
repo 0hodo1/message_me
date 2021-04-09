b8d3ae357af9935aeb2ab7434c2bb283103de3e4
Ömer Hodo <omerhodo@gmail.com>
2021-04-09 20:36:47 +0000

    message model created and some problem fixed

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'semantic-ui-sass', '~> 2.4', '>= 2.4.2.0'
 gem 'jquery-rails'
+gem 'bcrypt', '~> 3.1', '>= 3.1.16'
+gem 'hirb', '~> 0.7.3'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"omerhodo",password:"123456")
+User.create(username:"ycaliskan",password:"123456")
+User.create(username:"mcankaya",password:"123456")
+User.create(username:"hdilek",password:"123456")
